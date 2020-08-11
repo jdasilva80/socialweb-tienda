9c4759dda153007314772e25397b6a1f840f1a30
jdasilva80 <jdasilva1980@gmail.com>
2020-08-11 18:21:22 +0200

    message converters restclient

--- src/main/java/com/jdasilva/socialweb/tienda/app/domain/service/ProductoServiceImpl.java
+++ src/main/java/com/jdasilva/socialweb/tienda/app/domain/service/ProductoServiceImpl.java
@@ -1,12 +1,17 @@
 package com.jdasilva.socialweb.tienda.app.domain.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -47,6 +52,16 @@ public class ProductoServiceImpl implements IProductoService {
 		Map<String, String> pathVariables = new HashMap<>();
 		pathVariables.put("filename", filename);
 
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+		// Add the Jackson Message converter
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+		// Note: here we are making this converter to process any kind of response,
+		// not only application/*json, which is the default behaviour
+		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+		messageConverters.add(converter);
+		clienteRest.setMessageConverters(messageConverters);
+		
 		ResponseEntity<Resource> response = clienteRest.getForObject(
 				"https://soyjose-productos.herokuapp.com/apirest/productos/uploads/{filename}", ResponseEntity.class,
 				pathVariables);
